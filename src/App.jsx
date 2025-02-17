9c04366fff5045900fee242c745e76982f2846f7
Cameron Mohne <cameronmohne@gmail.com>
2025-02-17 12:30:03 -0800

    Revert back to working site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import './App.css'
-import { Home } from './best/Home'
+import './App.css';
+import { Home } from './best/Home';
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.gtag('config', 'G-LRHYLQ8PD4', {
-      page_path: location.pathname,
-    });
-  }, [location]);
-
   return (
     <>
       <Home />
